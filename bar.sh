3d182e8b38e45c6e8187c5a82b2c060f36677bc8
Kathryn AV Dambeck <audiovisual@dambeck.org>
2025-11-30 01:12:59 -0500

    fixed mpd status

--- bar.sh
+++ bar.sh
@@ -1,11 +1,12 @@
 while true
 do
-	mpd=$(if [ "$(mpc current)" != "" ]; then echo \| $(mpc current -f %artist%) - $(mpc current -f %album%) - $(mpc current -f %title%) - $(mpc status  %currenttime%/%totaltime%) - $(mpc current -f %position%)/$(mpc playlist | wc -l); fi) \| 
+
+	if mpc status | grep -q "playing"; then mpd="| $(mpc current -f %artist%) - $(mpc current -f %album%) - $(mpc current -f %title%) - $(mpc status  %currenttime%/%totaltime%) - $(mpc current -f %position%)/$(mpc playlist | wc -l)"; else mpd=""; fi
 	vol=$(pactl get-sink-volume @DEFAULT_SINK@ | head -n 1 | cut -d'/' -f2) 
 	date=$(date +'%Y-%m-%d | %R'); 
 	mem=$(free -h | awk '/Mem:/ {printf "%.1f/%.1fG\n", $3, $2}')
-	cpu=$(top -bn1 | grep "Cpu(s)" | awk '{printf "%02d%%\n", int(100 - $8)}')
+	cpu=$(top -bn1 | grep "Cpu(s)" | awk '{printf "%03d%%\n", int(100 - $8)}')
 
 	echo -e $mpd \| $mem $cpu \| vol $vol \| $date;
-	sleep 1;
+	sleep 0.3;
 done
