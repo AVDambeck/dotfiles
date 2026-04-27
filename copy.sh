7877d185445743b8fa0f2538b7df2abf82f43b96
avdambeck <audovisual@dambeck.org>
2026-04-27 15:15:02 -0400

    decreased gaps

--- copy.sh
+++ copy.sh
@@ -1,5 +1,8 @@
-cp -r ~/documents/git/dotfiles/config/* ~/.config/
-cp ~/documents/git/dotfiles/bashrc ~/.bashrc
-cp ~/documents/git/dotfiles/xinitrc ~/.xinitrc
+cd ~/documents/git/dotfiles/
+cp -r config/* ~/.config/
+cp bashrc ~/.bashrc
+cp xinitrc ~/.xinitrc
 sway reload 2> /dev/null
 i3-msg reload 2> /dev/null
+git add *
+git commit
